cdee66607bc06a8d942f43ff7b9313606791ee3d
Kerry Buckley <kerryjbuckley@gmail.com>
2012-06-17 09:30:28 +0100

    Move rack-proxy to middleware, so we can proxy everything under /crosswords

--- config.ru
+++ config.ru
@@ -2,21 +2,18 @@ require "sinatra"
 require "sinatra-twitter-oauth"
 require "./guardian_proxy"
 
-BASE_URL = "http://cruciverbalist.herokuapp.com"
+BASE_URL = ENV["base-url"] || "http://localhost:9292"
 
 set :twitter_oauth_config, key: ENV["consumer-key"],
   secret: ENV["consumer-secret"],
   callback: "#{BASE_URL}/auth",
   login_template: {text: %{<a href="#{BASE_URL}/connect">Sign in with Twitter</a>}}
 
+use GuardianProxy
 
 get "/" do
   login_required
   "Hello #{user.screen_name}!"
-end
-
-map('/crosswords') do
-  run GuardianProxy.new
 end
 
 run Sinatra::Application
--- guardian_proxy.rb
+++ guardian_proxy.rb
@@ -1,6 +1,20 @@
 require "rack-proxy"
+require "zlib"
 
 class GuardianProxy < Rack::Proxy
+  def initialize app
+    @app = app
+  end
+
+  def call env
+    request = Rack::Request.new env
+    if request.path =~ %r{^/crosswords/}
+      super
+    else
+      @app.call env
+    end
+  end
+
   def rewrite_env env
     env["HTTP_HOST"] = "www.guardian.co.uk"
     env["SERVER_PORT"] = 80
@@ -8,6 +22,16 @@ class GuardianProxy < Rack::Proxy
   end
 
   def rewrite_response triplet
-    triplet
+    status, headers, body = triplet
+    raw_body = if headers["content-encoding"] == "gzip"
+                 Zlib::GzipReader.new(StringIO.new(body.to_s),
+                                      :external_encoding => body.to_s.encoding).read
+               else
+                 body
+               end
+    new_body = raw_body.to_s.gsub "http://www.guardian.co.uk/crosswords/", "#{BASE_URL}/crosswords/"
+    headers["Content-Length"] = new_body.length.to_s
+    headers.delete "content-encoding"
+    [status, headers, [new_body]]
   end
 end
